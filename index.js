df7ae4961b380e85e7a7d56ccc65b9cb8947f4c7
fojletuhin <fojletuhin@gmail.com>
2024-10-11 22:09:24 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require ('express');
-const cors = require ('cors');
-require ('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -12,7 +12,8 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z7hla77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+
+// console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,20 +25,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
+    const usersCollection = client.db("TaskPlanet").collection('users');
+
+    app.get('/users', async(req, res)=>{
+      const result =await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
+run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('Task planet is running');
+app.get('/', (req, res) => {
+  res.send('Task planet is running');
 })
 
-app.listen(port,()=>{
-    console.log(`Task planet server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Task planet server is running on port ${port}`);
 })
 
 
